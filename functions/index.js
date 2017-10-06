ea748b49a657a66d586a3b93700154d2f6039495
Benjamin LACROIX <benjlacroix@gmail.com>
2017-10-06 16:56:06 +0200

    Set indent to 2 spaces

--- functions/index.js
+++ functions/index.js
@@ -6,24 +6,24 @@ const http = require('http');
 admin.initializeApp(functions.config().firebase);
 
 const action = {
-    SLOT_NEXT: 'slot.next',
-    TRAFFIC_BY_LINE: 'traffic.line',
+  SLOT_NEXT: 'slot.next',
+  TRAFFIC_BY_LINE: 'traffic.line',
 };
 
 const argument = {
-    METRO_LINE: 'metro_line',
+  METRO_LINE: 'metro_line',
 };
 
 const tellNextSlot = app => {
-    app.tell('<speak>Le prochain slot est<break time="200ms"/>Dans ton Chat à 10h30</speak>');
+  app.tell('<speak>Le prochain slot est<break time="200ms"/>Dans ton Chat à 10h30</speak>');
 };
 
 const tellTrafficByLine = app => {
+  // noinspection JSUnresolvedVariable
+  http.get(`http://api-ratp.pierre-grimaud.fr/v3/traffic/metros/13`, res => {
     // noinspection JSUnresolvedVariable
-    http.get(`http://api-ratp.pierre-grimaud.fr/v3/traffic/metros/13`, res => {
-        // noinspection JSUnresolvedVariable
-        app.tell(res.result.message);
-    });
+    app.tell(res.result.message);
+  });
 };
 
 const actionMap = new Map();
@@ -31,6 +31,6 @@ actionMap.set(action.SLOT_NEXT, tellNextSlot);
 actionMap.set(action.TRAFFIC_BY_LINE, tellTrafficByLine);
 
 exports.infoByXebia = functions.https.onRequest((request, response) => new ApiAiApp({
-    request,
-    response,
+  request,
+  response,
 }).handleRequest(actionMap));
