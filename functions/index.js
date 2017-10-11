196b7295b7de7d7b37dfdc6dc449c1b7af49ca06
Benjamin LACROIX <benjlacroix@gmail.com>
2017-10-11 15:59:21 +0200

    Add asset for closest station and menu

--- functions/index.js
+++ functions/index.js
@@ -23,16 +23,19 @@ const tellNextSlot = app => {
   app.ask('<speak>Le prochain slot est<break time="200ms"/>Dans ton Chat à 10h30</speak>');
 };
 
-const fireClosestStationEvent = app => {
+const tellClosestStation = app => {
+  app.ask(`La station Miromesnil est à 5 minutes`);
+  // noinspection JSIgnoredPromiseFromCall
   database.ref().child('content').update({
-    "url": "http://lasaintepaire.com/wp-content/uploads/2016/02/Fresque-Murale-Xebia-1920x587.png",
-    "type": "image/png"
+    url: 'https://i.imgur.com/OEHXFO3.png',
+    type: 'image/png',
   })
 };
 
 const tellTrafficByLine = app => {
   // noinspection JSUnresolvedVariable
-  https.get(`https://api-ratp.pierre-grimaud.fr/v3/traffic/metros/${app.getArgument(ARG.METRO_LINE)}`, res => {
+  const metroLine = app.getArgument(ARG.METRO_LINE);
+  https.get(`https://api-ratp.pierre-grimaud.fr/v3/traffic/metros/${metroLine}`, res => {
     res.setEncoding('utf8');
     let raw = '';
     res.on('data', chunk => raw += chunk);
@@ -41,7 +44,7 @@ const tellTrafficByLine = app => {
         const data = JSON.parse(raw);
         // noinspection JSUnresolvedVariable
         if (data.result.slug === 'normal') {
-          app.ask(`Le traffic est normal sur la ligne ${app.getArgument(ARG.METRO_LINE)}`);
+          app.ask(`Le traffic est normal sur la ligne ${metroLine}`);
         } else {
           app.ask(data.result.message);
         }
@@ -54,19 +57,24 @@ const tellTrafficByLine = app => {
 
 const tellWhatForLunch = app => {
   database.ref().child('lunch').once('value').then(res => {
-    const forLunch = getWhatForLunch(res.val());
+    const forLunch = getWhatForLunch(res.val()); // Get menu by day from firebase Db
     if (forLunch) {
       app.ask(forLunch);
     } else {
-      app.ask('Désolé, je \'ai pas trouvé de menu pour aujourd\'hui');
+      app.ask('Et voilà le menu');
     }
+    // noinspection JSIgnoredPromiseFromCall
+    database.ref().child('content').update({
+      url: 'https://i.imgur.com/txDXnub.png',
+      type: 'image/png'
+    });
   });
 };
 
 const actionMap = new Map();
 actionMap.set(ACTION.SLOT_NEXT, tellNextSlot);
 actionMap.set(ACTION.TRAFFIC_BY_LINE, tellTrafficByLine);
-actionMap.set(ACTION.CLOSEST_STATION, fireClosestStationEvent);
+actionMap.set(ACTION.CLOSEST_STATION, tellClosestStation);
 actionMap.set(ACTION.LUNCH, tellWhatForLunch);
 
 exports.infoByXebia = functions.https.onRequest((request, response) => new ApiAiApp({
